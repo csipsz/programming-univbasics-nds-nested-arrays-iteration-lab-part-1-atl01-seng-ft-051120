cf0bc66fa0b11ac3c33265cc0aa2ecf15f7c3c68
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-30 22:47:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ array_of_arrays = [
 
 def find_even_values(array_of_arrays)
   row_count = 0 
-  while row_count < array_of_arrays.length do 
+  while row_count < array_of_arrays.length
     column_count = 0 
     while column_count < array_of_arrays[row_count].length do 
       if array_of_arrays[row_count][column_count] % 2 == 0 do
