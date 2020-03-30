784c4d244e754bb6f6e965b174a5aa99bcd74472
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-30 22:42:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,11 @@
-def find_even_values(src)
+def find_even_values(array_of_arrays)
+  row_count = 0 
+  while row_count < array_of_arrays.length do 
+    column_count = 0 
+    while column_count < array_of_arrays[row_count].length do 
+      if do
+  end
+end
   
 
 end
